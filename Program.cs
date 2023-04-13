31c27ca1cec8ccb93f19e6771cd6725e46d26e5f
Dxxelisson2 <130613973+Dxxelisson2@users.noreply.github.com>
2023-04-12 21:44:15 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double expoente;
+            double basee;
+            double resultado;
+            
+            Console.WriteLine("digite o valor da base");
+            basee = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite o valor do expoente");
+            expoente = double.Parse(Console.ReadLine());
+
+            resultado = Math.Pow(basee,expoente) ;
+
+
+            Console.WriteLine("resultado é: "+resultado);
+
+        }
+    }
+}
